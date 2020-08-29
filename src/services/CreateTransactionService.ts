47648e081a58502d192cd42bec6102e3ff8f7f81
Renan Castro <renandf@gmail.com>
2020-08-30 07:51:06 +1000

    DB created; Create and List Transactions done.

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,66 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository, getRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+
+interface Request {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category?: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    type,
+    value,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const categoryRepository = getRepository(Category);
+
+    let transactionCategory;
+    let transactionDetails;
+
+    if (category === '') {
+      transactionDetails = {
+        title,
+        type,
+        value,
+      };
+    } else {
+      transactionCategory = await categoryRepository.findOne({
+        where: {
+          title: category,
+        },
+      });
+
+      if (!transactionCategory) {
+        transactionCategory = categoryRepository.create({
+          title: category,
+        });
+
+        await categoryRepository.save(transactionCategory);
+      }
+
+      transactionDetails = {
+        title,
+        type,
+        value,
+        category: transactionCategory,
+      };
+    }
+
+    const transaction = transactionsRepository.create(transactionDetails);
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
